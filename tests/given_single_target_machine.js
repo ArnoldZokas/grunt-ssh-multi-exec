328ff3209b2c2ba3d105c3a91fb1e97bb49dfc06
Arnold Zokas <arnold.zokas@coderoom.net>
2014-03-05 17:11:35 +0000

    setup test scaffold

--- tests/given_single_target_machine.js
+++ tests/given_single_target_machine.js
@@ -6,6 +6,15 @@ exports.when_executing_single_command = {
     it_should_execute_successfully: function(test) {
         test.expect(1);
 
+        grunt.initConfig({
+            'ssh-multi-exec': {
+                test: { }
+            }
+        });
+        require('../src/ssh-multi-exec')(grunt);
+        grunt.registerTask('default', ['ssh-multi-exec']);
+        grunt.task.run('default');
+
         test.equal(true, true);
 
         test.done();
--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,1 +1,4 @@
 apt-get update -y -q
+
+useradd test --create-home -s /bin/bash
+echo "test:test"|chpasswd
