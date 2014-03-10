4be4b063e6da35c75be9e6192bf602f6b8f32e80
Arnold Zokas <arnold.zokas@coderoom.net>
2014-03-10 14:16:50 +0000

    added tests to verify correct behaviour when command fails on multiple boxes[D

--- tests/given_multiple_target_machines.js
+++ tests/given_multiple_target_machines.js
@@ -5,7 +5,7 @@ var wait = function(test, num) {
         if(test._assertion_list.length === num) {
             test.done();
         } else {
-            wait();
+            wait(test, num);
         }
     }, 50);
 };
@@ -33,6 +33,69 @@ exports.when_executing_single_successfull_command = {
             }
         });
 
-        wait(test, 2);
+        wait.call(this, test, 2);
+    }
+};
+
+exports.when_executing_multiple_successfull_commands = {
+    it_should_execute_success_callback: function(test) {
+        test.expect(4);
+
+        var task = require('../src/grunt-ssh-multi-exec')(require('grunt'));
+        task.call({
+            target: 'echoes',
+            async: function(){ return function(){}; },
+            data: {
+                hosts: ['127.0.0.1:2222', '127.0.0.1:2223'],
+                username: 'vagrant',
+                privateKey: '/Users/' + process.env.USER + '/.vagrant.d/insecure_private_key',
+                commands: [
+                    {
+                        input: 'echo 2',
+                        success: function(data) {
+                            test.strictEqual(data, '2\n');
+                        }
+                    },
+                    {
+                        input: 'echo 3',
+                        success: function(data) {
+                            test.strictEqual(data, '3\n');
+                        }
+                    }
+                ]
+            }
+        });
+
+        wait.call(this, test, 4);
+    }
+};
+
+exports.when_executing_multiple_successfull_commands_and_first_command_fails = {
+    it_should_execute_success_callback: function(test) {
+        test.expect(2);
+
+        var task = require('../src/grunt-ssh-multi-exec')(require('grunt'));
+        task.call({
+            target: 'echoes',
+            async: function(){ return function(){}; },
+            data: {
+                hosts: ['127.0.0.1:2222', '127.0.0.1:2223'],
+                username: 'vagrant',
+                privateKey: '/Users/' + process.env.USER + '/.vagrant.d/insecure_private_key',
+                commands: [
+                    {
+                        input: 'batman',
+                        error: function(err) {
+                            test.strictEqual(err, 'bash: batman: command not found\n');
+                        }
+                    },
+                    {
+                        input: 'echo 1'
+                    }
+                ]
+            }
+        });
+
+        wait.call(this, test, 2);
     }
 };
