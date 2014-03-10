533fd429b7125a5a5f07d811e8c12618019c7024
Arnold Zokas <arnold.zokas@coderoom.net>
2014-03-10 11:49:08 +0000

    implemented single-machine execution with async

--- src/grunt-ssh-multi-exec.js
+++ src/grunt-ssh-multi-exec.js
@@ -1,82 +1,90 @@
 'use strict';
 
-var fs  = require('fs'),
-    ssh = require('ssh2'),
-    log = console.log;
+var fs    = require('fs'),
+    async = require('async'),
+    ssh   = require('ssh2'),
+    log   = console.log;
 
 var init = function() {
     var done = this.async(),
         target = this.target,
         config = this.data,
-        host = config.hosts[0].split(':')[0],
-        port = config.hosts[0].split(':')[1],
+        hosts = config.hosts,
         username = config.username,
         privateKey = config.privateKey,
         password = config.password,
         commands = config.commands,
         tunnel = new ssh();
 
-    tunnel.on('ready', function() {
-        log(('\n\nexecuting command set "' + target + '" against hosts: ' + config.hosts).underline);
+    async.parallel([
+        function(callback) {
+            var host = hosts[0].split(':')[0],
+                port = hosts[0].split(':')[1];
 
-        var executeCommand = function(command) {
-            var shellPrefix = (username + '@' + host + ':~$ ').cyan;
+            tunnel.on('ready', function() {
+                log(('\n\nexecuting command set "' + target + '" against hosts: ' + config.hosts).underline);
 
-            var input = command.input.toString(),
-                success = command.success || function(){},
-                error = command.error || function(){};
+                var executeCommand = function(command) {
+                    var shellPrefix = (username + '@' + host + ':~$ ').cyan;
 
-            log(shellPrefix + (input).yellow);
+                    var input = command.input.toString(),
+                        success = command.success || function(){},
+                        error = command.error || function(){};
 
-            tunnel.exec(input, function(err, stream) {
-                if (err) {
-                    throw err;
-                }
+                    log(shellPrefix + (input).yellow);
 
-                stream.on('data', function(data, extended) {
-                    data = data.toString();
-                    if(extended === 'stderr') {
-                        log(shellPrefix + data.red);
-                        error(data);
-                    } else {
-                        log(shellPrefix + data.green);
-                        success(data);
-
-                        if(commands.length > 0) {
-                            executeCommand(commands.shift());
+                    tunnel.exec(input, function(err, stream) {
+                        if (err) {
+                            throw err;
                         }
-                    }
-                });
 
-                stream.on('close', function() {
-                    done();
-                });
+                        stream.on('data', function(data, extended) {
+                            data = data.toString();
+                            if(extended === 'stderr') {
+                                log(shellPrefix + data.red);
+                                error(data);
+                                callback(null, null);
+                            } else {
+                                log(shellPrefix + data.green);
+                                success(data);
+
+                                if(commands.length > 0) {
+                                    executeCommand(commands.shift());
+                                } else {
+                                    callback(null, null);
+                                }
+                            }
+                        });
+                    });
+                };
+
+                executeCommand(commands.shift());
             });
-        };
 
-        executeCommand(commands.shift());
-    });
+            tunnel.on('error', function(err) {
+                log('Connection error: ' + err);
+                done();
+            });
 
-    tunnel.on('error', function(err) {
-        log('Connection error: ' + err);
+            if(privateKey) {
+                tunnel.connect({
+                    host: host,
+                    port: port,
+                    username: username,
+                    privateKey: fs.readFileSync(privateKey)
+                });
+            } else {
+                tunnel.connect({
+                    host: host,
+                    port: port,
+                    username: username,
+                    password: password
+                });
+            }
+        }
+    ], function(){
         done();
     });
-
-    if(privateKey) {
-        tunnel.connect({
-            host: host,
-            port: port,
-            username: username,
-            privateKey: fs.readFileSync(privateKey)
-        });
-    } else {
-        tunnel.connect({
-            host: host,
-            port: port,
-            username: username,
-            password: password
-        });
-    }
 };
 
 module.exports = function(grunt) {
