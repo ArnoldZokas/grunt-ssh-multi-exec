f3b553803de1f70c3e67217cdf430a801f59878d
Arnold Zokas <arnold.zokas@coderoom.net>
2014-11-26 12:07:56 +0000

    lint

--- tasks/ssh-multi-exec.js
+++ tasks/ssh-multi-exec.js
@@ -68,7 +68,7 @@ var init = function() {
                     });
 
                     stream.stderr.on('data', function(data) {
-                        if(data != null) {
+                        if(data !== null) {
                             if(lastError === null) {
                                 lastError = data.toString();
                             } else {
